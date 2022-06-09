704d8774efb00bf99e48ac12fee068b5ba2ec099
Meng Yuan <13714858@student.uts.edu.au>
2022-06-09 23:04:58 +0930

    fix login status

--- frontend/hooks/auth.js
+++ frontend/hooks/auth.js
@@ -45,10 +45,10 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
     setStatus(null);
 
     axios
-      .post("/login", props)
+      .post("/api/login", props)
       .then(() => mutate())
       .catch((error) => {
-        if (error.response.status !== 422) throw error;
+        if (error.response.status !== 201) throw error;
 
         setErrors(Object.values(error.response.data.errors).flat());
       });
