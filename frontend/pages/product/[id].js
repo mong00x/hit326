f5a41968a57002a433ef58b67ac5a43f36de7a1a
Meng Yuan <13714858@student.uts.edu.au>
2022-06-09 23:17:14 +0930

    add to cart from product page

--- frontend/pages/product/[id].js
+++ frontend/pages/product/[id].js
@@ -2,8 +2,9 @@ import React from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import styles from "../../styles/Home.module.css";
-import { Text, Button } from "@chakra-ui/react";
+import { Text, Button, Flex } from "@chakra-ui/react";
 import Nav from "../../components/navbar";
+import Context from "../../context/context";
 
 import useSWR from "swr";
 
@@ -15,36 +16,51 @@ const product = () => {
   const router = useRouter();
   const { id, name } = router.query;
 
+  const addToCart = () => {
+    console.log("add to cart");
+  };
+
   const { data: product, error } = useSWR(`/api/products/${id}`, fetcher);
   if (error) return <div>failed to load product</div>;
 
   if (!product) return <div>loading...</div>;
+
   return (
-    <div>
+    <>
       <Nav />
-      <main
-        className={styles.main}
-        style={{ flexDirection: "row", alignItems: "flex-start" }}
-      >
-        <Image
-          src={product.image}
-          alt={product.name}
-          width={500}
-          height={500}
-          className={styles.image}
-        />
-        <div className={styles.productInfo}>
-          <Text fontSize="4xl">{product.name}</Text>
-          <Text fontSize="md">{product.description}</Text>
-          <Text fontSize="xl">${product.price}</Text>
-          <Button>add to cart</Button>
-        </div>
-      </main>
+      <Context.Consumer>
+        {(context) => (
+          <main
+            className={styles.main}
+            style={{ flexDirection: "row", alignItems: "flex-start" }}
+          >
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={500}
+              height={500}
+              className={styles.image}
+            />
+            <Flex flexDirection={"column"}>
+              <Text fontSize="4xl">{product.name}</Text>
+              <Text fontSize="md">{product.description}</Text>
+              <Text fontSize="xl">${product.price}</Text>
+              <Button
+                onClick={() => {
+                  context.addProductToCart(product);
+                }}
+              >
+                Add to cart
+              </Button>
+            </Flex>
+          </main>
+        )}
+      </Context.Consumer>
 
       <footer className={styles.footer}>
         <p>HIT326 | Coded by Meng</p>
       </footer>
-    </div>
+    </>
   );
 };
 
