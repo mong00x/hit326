d4516652c1027726408e2e91056f3bf8dcfdcb88
Meng Yuan <13714858@student.uts.edu.au>
2022-06-10 19:54:53 +0930

    remove unused code

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -37,60 +37,33 @@ const NavLink = () => (
 
 export default function Nav() {
   const { colorMode, toggleColorMode } = useColorMode();
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const { user } = useAuth({ middleware: "guest" });
   return (
-    <div className="fixed min-w-full">
-      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
-        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Link href="/">Art Store</Link>
+    <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+      <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+        <Link href="/">Art Store</Link>
 
-          <Flex alignItems={"center"}>
-            <Stack direction={"row"} spacing={7} alignItems={"center"}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-              </Button>
-              <Cart />
-              <Menu>
-                {/* get user, if user logedin show dashboard, else show login and register */}
-                {user ? (
-                  <MenuButton
-                    as={Button}
-                    rounded={"full"}
-                    variant={"link"}
-                    cursor={"pointer"}
-                    minW={0}
-                  ></MenuButton>
-                ) : (
-                  <Box height="full">
-                    <Link href="/login">
-                      <a>Admin Login</a>
-                    </Link>
-                  </Box>
-                )}
-                <MenuList alignItems={"center"}>
-                  <br />
-                  <Center>
-                    <Avatar
-                      size={"2xl"}
-                      src={"https://avatars.dicebear.com/api/male/username.svg"}
-                    />
-                  </Center>
-                  <br />
-                  <Center>
-                    <p>Username</p>
-                  </Center>
-                  <br />
-                  <MenuDivider />
-                  <MenuItem>Your Servers</MenuItem>
-                  <MenuItem>Account Settings</MenuItem>
-                  <MenuItem>Logout</MenuItem>
-                </MenuList>
-              </Menu>
-            </Stack>
-          </Flex>
+        <Flex alignItems={"center"}>
+          <Stack direction={"row"} spacing={7} alignItems={"center"}>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+            </Button>
+            <Cart />
+
+            {typeof window !== "undefined" && window.localStorage.token ? (
+              <Link href="/dashboard">
+                <a>Dashboard</a>
+              </Link>
+            ) : (
+              <Box height="full">
+                <Link href="/login">
+                  <a>Admin Login</a>
+                </Link>
+              </Box>
+            )}
+          </Stack>
         </Flex>
-      </Box>
-    </div>
+      </Flex>
+    </Box>
   );
 }
